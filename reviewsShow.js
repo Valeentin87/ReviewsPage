cf4b29d0d095fb3d156d4dcf2d9e43e002336d89
Valeentin87 <Valeentin87@list.ru>
2024-09-24 17:07:46 +0300

    финальный коммит

--- reviewsShow.js
+++ reviewsShow.js
@@ -2,6 +2,34 @@ const mainContainer = document.querySelector('.container')
 let productKey;
 let productListReviews;
 
+let deleteParagraph;
+let deleteHeadProduct;
+
+localStorage.removeItem('Моооко');
+
+function delReviewFromStorage (product, review) {
+    for (let product_item = 0; product_item < localStorage.length; product_item++) {
+    productKey = localStorage.key(product_item);
+    if (productKey === product) {
+        console.log('найдено совпадение ключей')
+        let listReviews = JSON.parse(localStorage.getItem(productKey));
+        (listReviews.indexOf(review) == -1) ? console.log('что то пошло не так') : delete listReviews[listReviews.indexOf(review)];
+        console.log('отзыв удален!');
+        if (listReviews.length) {
+        listReviews = JSON.stringify(listReviews);
+        localStorage.setItem(productKey, listReviews);
+        }
+        else {
+            localStorage.removeItem(productKey);
+            console("все oтзывы по товару удалены");
+        }
+    }
+    }
+    
+};
+    
+
+
 
 
 function addDivBlockofProduct (productName, listReviews) {
@@ -13,6 +41,7 @@ function addDivBlockofProduct (productName, listReviews) {
     productContainer.appendChild(productHead);
     let divReview = document.createElement('div');
     let reviewParagraph;
+
     Array.from(listReviews).forEach(function (review) {
         
         divReview.className = 'div_review'
@@ -25,9 +54,16 @@ function addDivBlockofProduct (productName, listReviews) {
             buttonDel.textContent = 'Удалить отзыв';
             console.log('кнопка удаления создана');
             buttonDel.addEventListener('click', function() {
+                deleteHeadProduct = (this.parentElement).parentElement.firstChild.textContent.trim();
+                console.log((this.parentElement).parentElement.firstChild.textContent);
+                delReviewFromStorage(deleteHeadProduct, deleteParagraph);
+                this.previousElementSibling.remove();
+                this.remove();
+                
                 
             })
-            console.log(this.parentNode);
+            deleteParagraph = this.textContent.trim();
+            console.log(this.textContent);
             this.insertAdjacentElement('afterend', buttonDel);           
             }
         );
